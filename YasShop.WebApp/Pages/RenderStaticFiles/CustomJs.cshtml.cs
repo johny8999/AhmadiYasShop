67f8f735d723a32837f89d8a4fd5945654a6fcbe
sinaalipour77@hotmail.com <123sinaSINA>
2022-06-19 23:40:09 +0430

    70_

---
+++ YasShop.WebApp/Pages/RenderStaticFiles/CustomJs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YasShop.WebApp.Pages.RenderStaticFiles
+{
+    public class CustomJsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- YasShop.WebApp/wwwroot/shared/js/shared.js
+++ YasShop.WebApp/wwwroot/shared/js/shared.js
@@ -1,10 +1,32 @@
-﻿function ShowLoader() {
+﻿window.confirm = function (_text, _title, _yesAction = function () { }) {
+    swal.fire({
+        title: _title,
+        text: _text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: _yesText,
+        cancelButtonText: _noText
+    }).then((result) => {
+        if (result.isConfirmed) {
+            _yesAction();
+        }
+    })
+}
+
+function ShowLoader() {
     $('.ip-loader').fadeIn(100);
 }
 
 function HideLoader() {
     $('.ip-loader').fadeOut(100);
 }
+
+function ForgeryToken() {
+    return kendo.antiForgeryTokens()
+}
+
 function SendForm(_Url, _FormId, _CallBack = function (res) { }) {
     var form = $('#' + _FormId)[0];
     var _formData = new FormData(form);
@@ -42,7 +64,42 @@ function SendForm(_Url, _FormId, _CallBack = function (res) { }) {
     });
 }
 
-function ForgeryToken() {
-    
-    return kendo.antiForgeryTokens()
+function sendData(_Url, _data, _CallBack = function (res) { }) {
+    $.ajax({
+        type: "post",
+        enctype: "multipart/form-data",
+        url: _Url,
+        data: _data,
+        timeout: 600000,
+        beforeSend: function (xhr) {
+            ShowLoader();
+            var _token = $("[name=__RequestVerificationToken]").val();
+            xhr.setRequestHeader('XSRF-TOKEN', _token);
+        },
+        success: function (response) {
+            _CallBack(response)
+        },
+        complete: function (data) {
+            HideLoader();
+        },
+        error: function (err) {
+            if (err.status == 500) {
+                alert("Error 500");
+                return;
+            }
+            //else {
+            //    alert(err.status);
+            //}
+
+        }
+    });
 }
+
+
+
+function removeData(_url, _data = {}) {
+    confirm(_deleteMsg, "", function (){
+        sendData(_url,_data)
+    });
+}
+
