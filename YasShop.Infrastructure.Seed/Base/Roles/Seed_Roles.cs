83848200c697d0fe0501b09f007ca87222cb409f
johny jackson <sinaalipour77@hotmail.com>
2022-08-16 22:31:09 +0430

    94-95

--- YasShop.Infrastructure.Seed/Base/Roles/Seed_Roles.cs
+++ YasShop.Infrastructure.Seed/Base/Roles/Seed_Roles.cs
@@ -3,6 +3,8 @@ using Framework.Common.ExMethods;
 using Framework.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using YasShop.Domain.Users.RoleAgg.Contract;
 using YasShop.Domain.Users.RoleAgg.Entities;
@@ -43,19 +45,170 @@ public class Seed_Roles : ISeed_Roles
             #region Manage AccessLevel Page
             {
                 Guid _Id = new Guid().SequentialGuid();
-                if (await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanManageAccessLevel"))
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanManageAccessLevel"))
+                {
                     await _RoleRepository.AddAsync(new tblRoles
                     {
-                        Id=_Id,
-                        ParentId=null,
-                        PageName= "AccessLevelPage",
+                        Id = _Id,
+                        ParentId = null,
+                        PageName = "AccessLevelPage",
                         Name = "CanManageAccessLevel",
-                        NormalizedName= "CanManageAccessLevel".ToUpper(),
-                        Sort=10,
-                        Description="توانایی مدیریت سطوح دسترسی"
-                    },false);
+                        NormalizedName = "CanManageAccessLevel".ToUpper(),
+                        Sort = 10,
+                        Description = "توانایی مدیریت سطوح دسترسی"
+                    }, false);
+                }
+                else
+                {
+                    _Id = await _RoleRepository.GetNoTraking.Where(a => a.Name == "CanManageAccessLevel").Select(a => a.Id).SingleOrDefaultAsync();
+                }
+
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanViewListAccessLevel"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanViewListAccessLevel",
+                        NormalizedName = "CanViewListAccessLevel".ToUpper(),
+                        PageName = "AccessLevelPage",
+                        ParentId = _Id,
+                        Sort = 20,
+                        Description = "توانایی مشاهده سطح دسترسی"
+                    });
+                }
+
+                //Add
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanAddAccessLevel"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = _Id,
+                        ParentId = null,
+                        PageName = "AccessLevelPage",
+                        Name = "CanAddAccessLevel",
+                        NormalizedName = "CanAddAccessLevel".ToUpper(),
+                        Sort = 20,
+                        Description = "توانایی افزودن سطح دسترسی"
+                    }, false);
+                }
+
+                //Edit
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanEditAccessLevel"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = _Id,
+                        ParentId = null,
+                        PageName = "AccessLevelPage",
+                        Name = "CanEditAccessLevel",
+                        NormalizedName = "CanEditAccessLevel".ToUpper(),
+                        Sort = 30,
+                        Description = "توانایی ویرایش سطح دسترسی"
+                    }, false);
+                }
+
+                //Delete
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanDeleteAccessLevel"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanDeleteAccessLevel",
+                        NormalizedName = "CanDeleteAccessLevel".ToUpper(),
+                        PageName = "AccessLevelPage",
+                        ParentId = _Id,
+                        Sort = 40,
+                        Description = "توانایی حذف سطح دسترسی"
+                    });
+                }
+
+
             }
+
             #endregion Manage AccessLevel Page
+
+            #region Manage User Page
+            {
+                Guid _Id = new Guid().SequentialGuid();
+
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanManageUser"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = _Id,
+                        ParentId = null,
+                        PageName = "UserPage",
+                        Name = "CanManageUser",
+                        NormalizedName = "CanManageUser".ToUpper(),
+                        Sort = 10,
+                        Description = "توانایی مدیریت کاربران"
+                    }, false);
+                }
+                else
+                {
+                    _Id = await _RoleRepository.GetNoTraking.Where(a => a.Name == "CanManageAccessLevel").Select(a => a.Id).SingleOrDefaultAsync();
+                }
+                //EditUser
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanEditUser"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanEditUser",
+                        NormalizedName = "CanEditUser".ToUpper(),
+                        PageName = "CanEditUser",
+                        ParentId = _Id,
+                        Sort = 50,
+                        Description = "توانایی ویرایش کاربر"
+                    });
+                }
+
+                //DeleteUser
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanDeleteUser"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanDeleteUser",
+                        NormalizedName = "CanDeleteUser".ToUpper(),
+                        PageName = "AccessLevelPage",
+                        ParentId = _Id,
+                        Sort = 60,
+                        Description = "توانایی حذف کاربر"
+                    });
+                }
+
+                //LockUser
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanLockUser"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanLockUser",
+                        NormalizedName = "CanLockUser".ToUpper(),
+                        PageName = "AccessLevelPage",
+                        ParentId = _Id,
+                        Sort = 60,
+                        Description = "توانایی قفل حساب کاربر"
+                    });
+                }
+
+                //UnlockUser
+                if (!await _RoleRepository.GetNoTraking.AnyAsync(a => a.Name == "CanUnlockUser"))
+                {
+                    await _RoleRepository.AddAsync(new tblRoles
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        Name = "CanUnlockUser",
+                        NormalizedName = "CanUnlockUser".ToUpper(),
+                        PageName = "AccessLevelPage",
+                        ParentId = _Id,
+                        Sort = 60,
+                        Description = "توانایی باز کردن قفل حساب کاربر"
+                    });
+                }
+            }
+            #endregion
         }
         catch (ArgumentInvalidException ex)
         {
