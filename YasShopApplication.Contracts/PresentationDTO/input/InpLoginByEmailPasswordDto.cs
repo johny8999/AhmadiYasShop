e9103ed5f012cbe76bcb8f374e72131569d599b8
johny jackson <sinaalipour77@hotmail.com>
2022-11-08 23:05:33 +0330

    118-119

---
+++ YasShopApplication.Contracts/PresentationDTO/input/InpLoginByEmailPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Framework.Common.DataAnnotations.Strings;
+
+namespace YasShop.Application.Contracts.PresentationDTO.input;
+
+public sealed class InpLoginDto
+{
+    [Display(Name =nameof(UserId))]
+    [RequiredString]
+    [GUID]
+    public string UserId { get; set; }
+
+    [Display(Name =nameof(Password))]
+    [RequiredString]
+    public string Password { get; set; }
+}
---
+++ YasShopApplication.Contracts/PresentationDTO/input/InpRegisterByEmailPasswordDto.cs
@@ -0,0 +1,36 @@
+﻿using Framework.Common.DataAnnotations.Strings;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace YasShop.Application.Contracts.PresentationDTO.input;
+
+public sealed class InpRegisterByEmailPasswordDto
+{
+
+    [Display(Name = "FullName")]
+    [RequiredString]
+    [MaxLengthString(100)]
+    [FullName]
+    public string FullName { get; set; }
+
+    [Display(Name = "Email")]
+    [RequiredString]
+    [MaxLengthString(100)]
+    [Email]
+    public string Email { get; set; }
+
+    [Display(Name = "Password")]
+    [RequiredString]
+    [MaxLengthString(100)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+
+    /// <summary>
+    /// getting parameter with [TOKEN] variable
+    /// </summary>
+    [Display(Name = "ConfirmationLinkTemplate")]
+    [RequiredString]
+    [MaxLengthString(500)]
+    public string ConfirmationLinkTemplate { get; set; }
+}
